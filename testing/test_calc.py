587b316076ab65baf1345234e46e083197bea35f
BaoziShen <67185217+BaoziShen@users.noreply.github.com>
2020-07-10 19:18:35 +0800

    Update test_calc.py
    添加数据驱动

--- testing/test_calc.py
+++ testing/test_calc.py
@@ -1,6 +1,6 @@
 #测试文件
 import sys
-
+import yaml
 import pytest
 
 print(sys.path.append('..'))
@@ -38,46 +38,26 @@ class TestCalc:
     #     print("计算结束")
 
     @pytest.mark.add
-    @pytest.mark.parametrize('a,b,result',[
-    (1,2,3),
-    (-10,-20,-30),
-    (800,1600,2400),
-    (1.4,0.7,2.1)
-    ],ids=['int','minus','bigint','float']
+    @pytest.mark.parametrize('a,b,result', yaml.safe_load(open("./data1.yaml")),ids=['int','minus','bigint','float']
                              )
 
     def test_add(self,a,b,result,calcu):
         assert result ==self.cal.add(a,b)
 
     @pytest.mark.sub
-    @pytest.mark.parametrize('a,b,result', [
-        (1, 2, -1),
-        (-10, -20, 10),
-        (800, 1600, -800),
-        (1.4, 0.7, 0.7)
-    ],ids=['int','minus','bigint','float']
+    @pytest.mark.parametrize('a,b,result', yaml.safe_load(open("./data2.yaml")),ids=['int','minus','bigint','float']
                              )
     def test_sub(self,a,b,result,calcu):
         assert result == self.cal.sub(a,b)
 
     @pytest.mark.mul
-    @pytest.mark.parametrize('a,b,result', [
-        (1, 2, 2),
-        (-10, -20, 200),
-        (800, 1600, 1280000),
-        (1.4, 0.7, 0.98)
-    ],ids=['int','minus','bigint','float']
+    @pytest.mark.parametrize('a,b,result', yaml.safe_load(open("./data3.yaml")),ids=['int','minus','bigint','float']
                              )
     def test_mul(self,a,b,result,calcu):
         assert result == self.cal.mul(a,b)
 
     @pytest.mark.div
-    @pytest.mark.parametrize('a,b,result', [
-        (1, 2, 0.5),
-        (-10, -20, 0.5),
-        (800, 1600, 0.5),
-        (1.4, 0.7, 2)
-    ],ids=['int','minus','bigint','float']
+    @pytest.mark.parametrize('a,b,result', yaml.safe_load(open("./data4.yaml")),ids=['int','minus','bigint','float']
                              )
     def test_div(self,a,b,result,calcu):
         assert result == self.cal.div(a,b)
